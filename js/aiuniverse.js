ed748504089eb59d8fd119821f898104d0664377
Azmal <azmalgazi8@gmail.com>
2023-03-03 21:16:28 +0600

    uodate accuracy

--- js/aiuniverse.js
+++ js/aiuniverse.js
@@ -92,11 +92,12 @@ const loadAiDetails =async(id) =>{
 }
 
 const displayAiDetails = (tool) => {
-  console.log(tool
+  console.log(tool.accuracy
     );
 
    
   const apiDetails = document.getElementById("modal-details");
+  apiDetails.innerHTML = "";
 
   const div =document.createElement('div');
   div.innerHTML =`
@@ -132,7 +133,7 @@ ${tool.pricing[2]? tool.pricing[2].plan : "Not found" } </h1>
       </div>
       <div class="grid grid-cols-1 lg:grid-cols-2 gap-2 sm:mx-auto">
         <div>
-          <h1 class="font-bold mt-3">Features</h1>
+          <h1 class="font-bold mt-3 text-2xl">Features</h1>
           <ul class="list-disc ml-6 mt-3 ">
             <li>${tool.features[1]? tool.features[1].feature_name : "Not Found"}</li>
   <li>${tool.features[2]? tool.features[2].feature_name : "Not Found"}</li>
@@ -141,7 +142,7 @@ ${tool.pricing[2]? tool.pricing[2].plan : "Not found" } </h1>
           </ul>
         </div>
         <div>
-          <h1 class="font-bold mt-3">Integrations</h1>
+          <h1 class="font-bold mt-3 text-2xl">Integrations</h1>
           <ul class="list-disc ml-6 mt-3  ">
             <li>${tool.integrations[0] ? tool.integrations[0] : "Not Found"}</li>
             <li>${tool.integrations[1] ? tool.integrations[1] : "Not Found"}</li>
@@ -155,13 +156,16 @@ ${tool.pricing[2]? tool.pricing[2].plan : "Not found" } </h1>
   <div
     class="card w-96 bg-base-100 shadow-xl border rounded-lg border-gray-200 mx-auto"
   >
-    <figure><img class="relative" src="${tool.image_link[0] ? tool.image_link[0] : tool.image_link[0] }" alt="Shoes" /></figure>
+    <figure class="p-7"><img class="relative border rounded-lg w-100 h-96" src="${tool.image_link[0] ? tool.image_link[0] : tool.image_link[0] }" alt="Shoes" /></figure>
     <div class="card-body">
-      <h2 class="font-3xl font-bold text-center ">Hi,how are you doing today?</h2>
-      <p class="text-center">Lorem ipsum dolor sit amet, consectetur</p>
-      <div class="card-actions justify-end absolute top-1 right-1">
-        <button class="accuracy-btn border rounded-lg btn-primary p-2 hidden">
-          95% accuracy
+      <h2 class="font-3xl font-bold text-center text-2xl ">
+      ${tool.input_output_examples ? tool.input_output_examples[0].input : "Can you give any examples?"}</h2>
+      <p class="text-center">
+      ${tool.input_output_examples ? tool.input_output_examples[0].output : "No! Not Yet! Take a break"}
+      </p>
+      <div class="card-actions justify-end absolute top-9 right-9">
+        <button class="accuracy-btn  rounded-lg bg-rose-600 p-2 text-white">
+        ${tool.accuracy.score != null ? tool.accuracy.score * 100 + '% accuracy' : "null"}
         </button>
       </div>
     </div>
